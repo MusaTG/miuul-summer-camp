a771168a6deda8aba951ea8014743c1ffa41897d
MusaTG <49027797+MusaTG@users.noreply.github.com>
2023-07-22 17:49:24 +0300

    Add numpy operation

---
+++ Week1_PythonProgrammingforDataScience/data_analysis_with_python.py
@@ -0,0 +1,167 @@
+##############################################################################
+# PYTHON İLE VERİ ANALİZİ (DATA ANALYSIS WITH PYTHON
+##############################################################################
+# - NumPy
+# - Pandas
+# - Veri Görselleştirme: Matplotlib & Seaborn
+# - Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional Exploratory Data Analysis)
+
+
+################################################
+# NUMPY
+################################################
+
+# Neden NumPy? (Why NumPy?)
+# NumPy Array'i Oluşturmak (Creating NumPy Arrays)
+# NumPy Array Özellikleri (Attibutes of NumPy Arrays)
+# Yeniden Şekillendirme (Reshaping)
+# Index Seçimi (Index Selection)
+# Slicing
+# Fancy Index
+# NumPy'da Koşullu İşlemler (Conditions on Numpy)
+# Matematiksel İşlemler (Mathematical Operations)
+
+import numpy as np
+
+###############################
+# Neden NumPy?
+###############################
+
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+
+a * b
+
+###############################
+# NumPy Array'i Oluşturmak
+###############################
+
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5]))
+
+np.zeros(10, dtype=int)
+np.random.randint(0, 10, size=10)
+np.random.normal(10, 4, (3, 4))
+
+
+###############################
+# NumPy Array Özellikleri
+###############################
+# ndim: boyut sayısı
+# shape: boyut bilgisi
+# size: toplam eleman sayısı
+# dtype: array veri tipi
+
+a = np.random.randint(10, size=5)
+a.ndim
+a.shape
+a.size
+a.dtype
+
+
+###############################
+# Yeniden Şekillendirme (Reshaping)
+###############################
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size=9)
+ar.reshape(3, 3)
+
+
+###############################
+# Index Seçimi (Index Selection)
+###############################
+
+a = np.random.randint(10, size=10)
+a[0]
+a[0:5]
+a[0] = 999
+
+m = np.random.randint(10, size=(3, 5))
+
+m[0, 0]
+m[2, 3] = 999
+m[2, 3] = 2.9 # 2 olarak kaydeder fix type yani sabit tip np!!!
+
+m[:, 0]
+m[1:, ]
+m[0:2, 0:3]
+
+###############################
+# Fancy Index
+###############################
+
+v = np.arange(0, 30, 3)
+v[1]
+v[4]
+
+catch = [1, 2, 3]
+v[catch]
+
+
+###############################
+# NumPy'da Koşullu İşlemler (Conditions on Numpy)
+###############################
+
+v = np.array([1, 2, 3, 4, 5])
+
+##########################
+# Klasik döngü ile
+##########################
+
+ab = []
+for i in v:
+    if i < 3:
+        ab.append(i)
+
+##########################
+# NumPy ile
+##########################
+v < 3
+v[v < 3]
+v[v > 3]
+v[v != 3]
+v[v == 3]
+v[v >= 3]
+
+
+###############################
+# Matematiksel İşlemler (Mathematical Operations)
+###############################
+
+v = np.array([1, 2, 3, 4, 5])
+
+v / 5
+v * 5 / 10
+v ** 2
+v - 1
+
+np.subtract(v, 1)
+np.add(v, 1)
+np.mean(v)
+np.sum(v)
+np.min(v)
+np.max(v)
+np.var(v)
+
+###############################
+# NumPy ile iki bilinmeyenli denklem çözümü
+###############################
+
+# 5*x0 + x1 = 12
+# x0 + 3*x1 = 10
+
+a = np.array([[5, 1], [1, 3]])
+b = np.array([12, 10])
+
+np.linalg.solve(a, b)
